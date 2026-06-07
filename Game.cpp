581338675f756500168e943be87fa06108c002d7
Tyler <tylern247@gmail.com>
2026-06-06 22:31:29 -0500

    TODO #4 and #5: Updated Collision Check and Removal Check with new vector

--- Game.cpp
+++ Game.cpp
@@ -88,13 +88,23 @@ void Game::Render() const
 void Game::CheckCollision()
 {
 	// TODO #4 - Update collision to check all bricks
-	if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+	
+	// Iterate through bricks for collision check
+	for (auto& brick : bricks)
 	{
-		brick.color = ConsoleColor(brick.color - 1);
-		ball.y_velocity *= -1;
+		if (brick.Contains(ball.x_position + ball.x_velocity, ball.y_position + ball.y_velocity))
+		{
+			brick.color = ConsoleColor(brick.color - 1);
+			ball.y_velocity *= -1;
 
 		// TODO #5 - If the ball hits the same brick 3 times (color == black), remove it from the vector
-
+			
+			// Black Color Check for brick removal
+			if (brick.color == ConsoleColor::Black)
+			{
+				bricks.erase(std::remove(bricks.begin(), bricks.end(), brick), bricks.end());
+			}
+		}
 	}
 
 	// TODO #6 - If no bricks remain, pause ball and display (render) victory text with R to reset
