3dcabad07f1a0dcbbd3c40434cccaebc9611f4bb
Tyler <tylern247@gmail.com>
2026-06-06 20:09:36 -0500

    TODO #2/3 Made vector add 5 bricks and spaced them out

--- Game.cpp
+++ Game.cpp
@@ -20,12 +20,20 @@ void Game::Reset()
 	ResetBall();
 
 	// TODO #2 - Add this brick and 4 more bricks to the vector
-	brick.width = 10;
-	brick.height = 2;
-	brick.x_position = 0;
-	brick.y_position = 5;
-	brick.doubleThick = true;
-	brick.color = ConsoleColor::DarkGreen;
+	bricks.clear(); // Clear vector before adding new ones
+
+	// Iterate 5 times to create 5 bricks
+	for auto (i = 0; i < 5; ++i)
+	{
+		Box newBrick;
+		newBrick.width = 10;
+		newBrick.height = 2;
+		newBrick.x_position = i * 12; // Position bricks with some spacing (TODO #3?)
+		newBrick.y_position = 5;
+		newBrick.doubleThick = true;
+		newBrick.color = ConsoleColor::DarkGreen;
+		bricks.push_back(newBrick);
+	}
 }
 
 void Game::ResetBall()
